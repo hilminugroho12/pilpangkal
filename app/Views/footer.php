c5968677e100ba279d9711db17c27cee40ceef8b
j3c118154 <71500163+j3c118154@users.noreply.github.com>
2020-12-26 09:13:04 +0700

    benerin navbarnya

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -105,16 +105,16 @@
 </body>
 
 <!--   core js files    -->
-<script src="js/jquery.min.js" type="text/javascript"></script>
-<script src="js/bootstrap.js" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/js/jquery.min.js') ?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/js/bootstrap.js') ?>" type="text/javascript"></script>
 
 <!--  js library for devices recognition -->
-<script type="text/javascript" src="js/modernizr.js"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/modernizr.js') ?>"></script>
 
 <!--  script for google maps   -->
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
 
 <!--   file where we handle all the script from the Gaia - Bootstrap Template   -->
-<script type="text/javascript" src="js/gaia.js"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/gaia.js') ?>"></script>
 
 </html>
